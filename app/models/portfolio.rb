d38902f9236193c1c5c51444700408aa501374ef
Eslam Mohamed <eslammoh.ce@gmail.com>
2020-01-05 00:37:42 +0300

    add technology model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
   includes Placeholder
+  has_many :technologies
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
