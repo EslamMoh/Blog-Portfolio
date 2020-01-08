b320ed444d229af131e426825284331cace5b500
Eslam Mohamed <eslammoh.ce@gmail.com>
2020-01-08 22:06:01 +0300

    override current_user in controller concern

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,15 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+  
+  def current_user
+    # super : if current user exist return it , and if it is not logged in , use openstruct to simulate like a user object
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: 'Guest User',
+                   first_name: 'Guest',
+                   last_name: 'User',
+                   email: 'guest_user@gmail.com')
+  end
+end
