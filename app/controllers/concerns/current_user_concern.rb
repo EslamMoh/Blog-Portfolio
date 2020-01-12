58003d321432462e417e2b6ef7141d2f4273071c
Eslam Mohamed <eslammoh.ce@gmail.com>
2020-01-12 19:51:38 +0300

    update application layouts and add custom layouts

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,6 +1,6 @@
 module CurrentUserConcern
   extend ActiveSupport::Concern
-  
+
   def current_user
     # super : if current user exist return it , and if it is not logged in , use openstruct to simulate like a user object
     super || guest_user
